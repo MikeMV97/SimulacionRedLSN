5b7cab2eabcba43084f7c381bb296efd23ea9d1c
MiguelMoV <miga.moralesv@gmail.com>
2021-11-25 18:40:30 -0600

    Avanzando en funcionalidad de simulacion

--- simulacion-red.py
+++ simulacion-red.py
@@ -1,5 +1,8 @@
 from math import log10
+import types
+from typing import List, Tuple
 import numpy as np
+from numpy.random import default_rng
 """
 CONSIDERACIONES
 	* Empezar por nodo y grado mas alejado. (Primero Grado 7!!!)
@@ -43,30 +46,36 @@ Xi		= 18		# Num. ranuras de tiempo estado Sleeping
 N		= [5, 10, 15, 20]				# Num Nodos por Grado
 W		= [16, 32, 64, 128, 256]		# Num Max de miniranuras
 Lambda	= [0.0005, 0.001, 0.005, 0.03]	# Tasa de generacion de pkts por grado
-sink 		= 1		# ID nodo sink
-N_case = N[0]
+sink 	= 1			# ID nodo sink
+N_case	= N[0]
+Lambda_case = Lambda[0]
+
 
 def simulacion():
 	# INICIALIZACION VARIABLES DE SIMULACION:
 	t_sim		= 0						# Este siempre ira avanzando (No se mide de forma continua, solo queremos ir midiendo instantes en que se van generando los eventos)
 	t_arribo	= 0						# Nuevo tiempo de arribo, con esto encontramos el siguiente tiempo en que se genera un arribo
 	t_slot		= DIFS + 3*SIFS + (Sigma*W[0]) + t_RTS + t_CTS + t_ACK + t_DATA
-	Tc 			= (2 + Xi) * t_slot
+	Tc 			= (2 + Xi) * t_slot		# Tiempo_Tx + Tiempo_Rx + Tiempo_Sleeping
 
-	buffer	= np.zeros( (H, N_case), dtype=np.int32 )
-
+	nodos_perdidos	= np.zeros( (H,1), dtype=np.int32 )
+	buffer 		= [[[]]*N_case]*(H+1)  # Para cada nodo hay una lista de Pkts (se almacena time de creacion)
+	# print(np.shape(buffer))		# buffer[GRADO][NODO] = LIST(ta_NODO1,ta_NODO2,...)
+	
 	while t_sim < 10000*Tc:
 		while t_arribo < t_sim : # Ya ocurrio un arribo en la red
+			print('ta:', t_arribo, ' | tsim:', t_sim)
 			# Se_asigna_el_pkt_recibido_a_un_nodo_en_un_grado
 			grado_rand, nodo_rand = select_grade_and_node()
-			if buffer[grado_rand][nodo_rand] < K:
-				buffer[grado_rand][nodo_rand] += 1
+			if len(buffer[grado_rand][nodo_rand]) < K:
+				buffer[grado_rand][nodo_rand].append(t_arribo)
 			else:
-				nodos_perdidos_memoria[grado_rand] += 1
+				nodos_perdidos[grado_rand] += 1
 
 			# Se define un nuevo tiempo de arribo
 			t_arribo = generate_arrival_time(t_sim)
 
+		# test(buffer, nodos_perdidos)
 		
 		for grado_i in range(H, -1, 1):
 			# Verificar cuantos nodos tienen un buffer con datos.
@@ -87,43 +96,63 @@ def simulacion():
 		t_sim += t_slot
 
 
-def insertar_pkt_recibido(grado):
-	if grado == sink: # Pkt llego a destino
-		Tomar estadistica, pkt exitoso
-	else:
-		aumentar buffer de nodo
+rng_generacion_pkt_grado = default_rng()
+rng_generacion_pkt_nodo = default_rng()
+def select_grade_and_node():
+	grade = rng_generacion_pkt_grado.integers(1, H + 1, dtype=np.int32)
+	node = rng_generacion_pkt_nodo.integers(0, N_case, dtype=np.int32)
+	return grade, node
+
+
+rng_arrival = default_rng()
+VA_uniforme	= rng_arrival.uniform(0., 1., 100_000_000)
+VA_i = 0
+def generate_arrival_time(current_time):
+	global VA_i
+	nuevo_t		= - (1 / Lambda_case) * log10(1 - VA_uniforme[VA_i])  # Siguiente intervalo de tiempo en que se va a generar un pkt
+	VA_i += 1
+	return ( current_time + nuevo_t )
 
 
 def get_nodes_with_data(grado_i, buffer):
 	nodes = []
 	for node in range(0, N_case):
-		if(buffer[grado_i][node] > 0):
+		if(len(buffer[grado_i][node]) > 0):
 			nodes.append(node)
-	return nodes
 
 
 def contencion_del_canal(nodos_contendientes):
 	# 	1) COMO SE ELIGE EL NODO GANADOR?
 	# 	2) HAY COLISION ENTRE NODOS DEL MISMO GRADO Y/O DE DIFERENTE GRADO?
 	# 	3) QUE PASA CUANDO COLISIONAN LOS NODOS GANADOREs?
-	nodos = []
-	for nodo in nodos_contendientes:
-		backoff_time = rand
-		nodos.append({backoff_time, nodo})
-	nodos.sort()
-	nodo_ganador = nodos[0][1]
-	return nodo_ganador
-
-def select_grade_and_node():
-	grade = rand(1, H)
-	node = rand(0, N_case)
-	return grade, node
+	# nodos = []
+	# for nodo in nodos_contendientes:
+	# 	backoff_time = rng_arrivalintegers()
+	# 	nodos.append({backoff_time, nodo})
+	# nodos.sort()
+	# nodo_ganador = nodos[0][1]
+	idx_ganador = rng_arrival.integers(0, len(nodos_contendientes))
+	return nodos_contendientes[idx_ganador]	
 
 
-def generate_arrival_time(current_time):
-	VA_uniforme	= (1e6 * rand) / 1e6	# Aqui suponemos que rand es de distr. uniforme
-	nuevo_t		= - (1 / Lambda) * log10(1 - VA_uniforme)  # Siguiente intervalo de tiempo en que se va a generar un pkt
-	return ( current_time + nuevo_t )
+# def insertar_pkt_recibido(grado):
+# 	if grado == sink: # Pkt llego a destino
+# 		Tomar estadistica, pkt exitoso
+# 	else:
+# 		aumentar buffer de nodo
+
+
+def test(buffer, nodos_perdidos):
+	print('buffer:', np.shape(buffer), 'perdidos:', np.shape(nodos_perdidos))
+
+
+def len_buff(buffer):
+	print(type(buffer))
+	print(np.shape(buffer))
+	count = 0
+	for elem in buffer:
+		count += 1
+	return count
 
 
 if __name__ == '__main__':
